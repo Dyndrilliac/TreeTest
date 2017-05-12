fe093f1039237a00813b950b58a9a3d6de108877
Matthew Boyette <Dyndrilliac@gmail.com>
2017-05-12 01:35:56 -0400

    Update TreeTest.java

--- TreeTest.java
+++ TreeTest.java
@@ -7,6 +7,7 @@
  */
 
 import api.util.datastructures.Tree;
+import api.util.datastructures.TreeNode.TRAVERSAL_ORDER;
 
 public class TreeTest
 {
@@ -21,13 +22,12 @@ public class TreeTest
 
     public static void deletions(final Tree<Integer> t)
     {
-        TreeTest.delete(t, -4);
-        TreeTest.delete(t, 3);
-        TreeTest.delete(t, 17);
-        TreeTest.delete(t, 21);
-        TreeTest.delete(t, 2);
-        TreeTest.delete(t, 18);
-        TreeTest.delete(t, 5);
+        /*
+         * TreeTest.delete(t, 90);
+         * TreeTest.delete(t, 30);
+         * TreeTest.delete(t, 80);
+         * TreeTest.delete(t, 40);
+         */
     }
 
     public static void display(final Tree<Integer> t)
@@ -42,25 +42,80 @@ public class TreeTest
         StdOut.printf("%-20s: %s%n", insert, t.insert(n));
     }
 
-    public static void insertions(final Tree<Integer> t)
+    public static void insertions_b(final Tree<Integer> t)
     {
-        TreeTest.insert(t, 5);
-        TreeTest.insert(t, 2);
-        TreeTest.insert(t, 18);
-        TreeTest.insert(t, -4);
-        TreeTest.insert(t, 3);
-        TreeTest.insert(t, 17);
-        TreeTest.insert(t, 21);
+        /* Balanced BST */
+        // 60,40,80,50,70,90,30
+        TreeTest.insert(t, 60);
+        TreeTest.insert(t, 40);
+        TreeTest.insert(t, 80);
+        TreeTest.insert(t, 50);
+        TreeTest.insert(t, 70);
+        TreeTest.insert(t, 90);
+        TreeTest.insert(t, 30);
+        StdOut.println();
+    }
+
+    public static void insertions_l(final Tree<Integer> t)
+    {
+        /* Left-Skewed BST */
+        // 90,80,70,60,50,40,30
+        TreeTest.insert(t, 90);
+        TreeTest.insert(t, 80);
+        TreeTest.insert(t, 70);
+        TreeTest.insert(t, 60);
+        TreeTest.insert(t, 50);
+        TreeTest.insert(t, 40);
+        TreeTest.insert(t, 30);
+        StdOut.println();
+    }
+
+    public static void insertions_r(final Tree<Integer> t)
+    {
+        /* Right-Skewed BST */
+        // 30,40,50,60,70,80,90
+        TreeTest.insert(t, 30);
+        TreeTest.insert(t, 40);
+        TreeTest.insert(t, 50);
+        TreeTest.insert(t, 60);
+        TreeTest.insert(t, 70);
+        TreeTest.insert(t, 80);
+        TreeTest.insert(t, 90);
         StdOut.println();
     }
 
     public static void main(String[] args)
     {
-        Tree<Integer> t = new Tree<Integer>();
+        Tree<Integer> t;
+
+        t = new Tree<Integer>();
         TreeTest.display(t);
-        TreeTest.insertions(t);
+        TreeTest.insertions_b(t);
         TreeTest.display(t);
         TreeTest.deletions(t);
+        StdOut.println(t.toString(TRAVERSAL_ORDER.DEFAULT));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.IN));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.PRE));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.POST));
+
+        t = new Tree<Integer>();
         TreeTest.display(t);
+        TreeTest.insertions_r(t);
+        TreeTest.display(t);
+        TreeTest.deletions(t);
+        StdOut.println(t.toString(TRAVERSAL_ORDER.DEFAULT));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.IN));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.PRE));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.POST));
+
+        t = new Tree<Integer>();
+        TreeTest.display(t);
+        TreeTest.insertions_l(t);
+        TreeTest.display(t);
+        TreeTest.deletions(t);
+        StdOut.println(t.toString(TRAVERSAL_ORDER.DEFAULT));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.IN));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.PRE));
+        StdOut.println(t.toString(TRAVERSAL_ORDER.POST));
     }
 }
